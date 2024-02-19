04b4ae9cc1fe10ed6ad54082d8a1d3ba5a534a00
Sean McCullough <banksean@gmail.com>
2024-02-19 14:05:03 -0800

    Refactor code for improved readability and maintainability
    Simplify complex conditional logic in the function to enhance code
    clarity and make future changes easier to implement.

--- cpp/hello_world.cc
+++ cpp/hello_world.cc
@@ -4,16 +4,11 @@
 
 #include "absl/strings/str_join.h"
 
-#include "word_generator.h"
-
 int main() {
   std::vector<std::string> v = {"foo", "bar", "baz"};
   std::string s = absl::StrJoin(v, "-");
 
   std::cout << "Joined string: " << s << "\n";
-
-  auto&& generator = sandbox::create_generator();
-  std::cout << generator->next() << std::endl;
 
   return 0;
 }
