2c02d2871496e9eb51dee3d39261e2dd3fe1ecc8
Sean McCullough <banksean@gmail.com>
2024-02-27 17:13:03 -0800

    .

--- langchain/demo.py
+++ langchain/demo.py
@@ -1,12 +1,16 @@
-from langchain.llms import Ollama
-from langchain.document_loaders import WebBaseLoader
+from langchain_community.llms import Ollama
+from langchain_community.document_loaders import WebBaseLoader
 from langchain.chains.summarize import load_summarize_chain
 
 loader = WebBaseLoader("https://ollama.com/blog/run-llama2-uncensored-locally")
 docs = loader.load()
+print(f"docs: {docs}\n")
 
-llm = Ollama(model="llama2")
+llm = Ollama(model="llama2:latest", verbose=True, timeout=10) # timeout parameter is seconds.
+print(f"llm: {llm}\n")
+
 chain = load_summarize_chain(llm, chain_type="stuff")
+print(f"chain: {chain}\n")
 
-result = chain.run(docs)
+result = chain.invoke(docs)
 print(result)
